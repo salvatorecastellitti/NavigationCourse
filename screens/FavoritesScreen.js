ef324355d29098ed0e8585aca2dcc8ccb3a7511e
Salvatore Castellitti <s.castellitti@gmail.com>
2023-03-09 00:13:17 +0100

    Added Drawer

---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function FavoritesScreen() {
+  return (
+    <View>
+      <Text>Favorite screen</Text>
+    </View>
+  );
+}
+
+export default FavoritesScreen;
